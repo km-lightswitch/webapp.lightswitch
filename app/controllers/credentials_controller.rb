2d1dd93eb05f400bbef6f6aa982a44ec1837636b
Krishnan Mani <kmani@equalexperts.com>
2015-07-12 16:53:42 +0530

    Added validation to AWS credentials

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -1,6 +1,7 @@
 class CredentialsController < ApplicationController
 
   def new
+    @credential = Credential.new
   end
 
   def index
@@ -8,14 +9,22 @@ class CredentialsController < ApplicationController
   end
 
   def create
-    @credential = Credential.new(params.require(:credential).permit(:name, :access_key_id, :secret_access_key))
+    @credential = Credential.new(credential_params)
 
-    @credential.save
-    redirect_to action: 'show', id: @credential.id
+    if @credential.save
+      redirect_to action: 'show', id: @credential.id
+    else
+      render 'new'
+    end
   end
 
   def show
     @credential = Credential.find(params[:id])
   end
+
+  private
+    def credential_params
+      params.require(:credential).permit(:name, :access_key_id, :secret_access_key)
+    end
 
 end
